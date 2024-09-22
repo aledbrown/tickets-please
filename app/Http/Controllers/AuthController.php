80e76278fa82ce11e02a30d3d4827929d6001195
Aled Brown <690500+aledbrown@users.noreply.github.com>
2024-09-22 17:32:27 +0100

    Fake some requests

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,18 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApiLoginRequest;
 use App\Traits\ApiResponses;
 
 class AuthController extends Controller
 {
     use ApiResponses;
 
-    public function login()
+    public function login(ApiLoginRequest $request)
     {
-        return $this->ok('Hello, Login!', 200);
-        // return response()->json([
-        //     'name' => config('app.name'),
-        //     'message' => 'Hello, Login!',
-        // ], 200);
+        return $this->ok($request->get('email'));
+
+        // return $this->ok('Hello, Login!', 200);
+    }
+
+    public function register()
+    {
+        return $this->ok('Hello, Register!', 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //:Remember api/
+// https://tickets-please.test/api/
+// universal resource locator
+// tickets/{id}/edit
+// users
 
-Route::get('/login', [\App\Http\Controllers\AuthController::class, 'login']);
 
-Route::get('/', function () {
-    return response()->json([
-        'name' => config('app.name'),
-        'message' => 'Hello, API!',
-    ], 200);
-});
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/', function () {
+//     return response()->json([
+//         'name' => config('app.name'),
+//         'message' => 'Hello, API!',
+//     ], 200);
+// });
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');

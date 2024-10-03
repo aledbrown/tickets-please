56a8a0d351f46f0e272b818b951e4823f92bb562
Aled Brown <690500+aledbrown@users.noreply.github.com>
2024-10-03 15:46:37 +0100

    Ticket JSON optional include=author

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -8,10 +8,13 @@ use App\Http\Requests\Api\V1\UpdateUserRequest;
 use App\Http\Resources\V1\UserResource;
 use App\Models\User;
 
-class UserController extends Controller
+class UserController extends ApiController
 {
     public function index()
     {
+        if ($this->include('tickets')) {
+            return UserResource::collection(User::with('tickets')->paginate());
+        }
         return UserResource::collection(User::paginate());
     }
 
@@ -22,6 +25,9 @@ class UserController extends Controller
 
     public function show(User $user)
     {
+        if ($this->include('tickets')) {
+            return new UserResource($user->load('tickets'));
+        }
         return new UserResource($user);
     }
 

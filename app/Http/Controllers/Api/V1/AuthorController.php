8ac1b9cde381143274fc1080f1d543cb80802c39
Aled Brown <690500+aledbrown@users.noreply.github.com>
2024-10-10 14:21:09 +0100

    Refactor users to authors

--- app/Http/Controllers/Api/V1/AuthorController.php
+++ app/Http/Controllers/Api/V1/AuthorController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\StoreUserRequest;
 use App\Http\Requests\Api\V1\UpdateUserRequest;
 use App\Http\Resources\V1\UserResource;
 use App\Models\User;
 
-class UserController extends ApiController
+class AuthorController extends ApiController
 {
     public function index()
     {
@@ -23,12 +22,12 @@ class UserController extends ApiController
         //
     }
 
-    public function show(User $user)
+    public function show(User $author)
     {
         if ($this->include('tickets')) {
-            return new UserResource($user->load('tickets'));
+            return new UserResource($author->load('tickets'));
         }
-        return new UserResource($user);
+        return new UserResource($author);
     }
 
     public function update(UpdateUserRequest $request, User $user)

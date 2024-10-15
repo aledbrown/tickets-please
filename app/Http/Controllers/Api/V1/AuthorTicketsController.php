3c0d738acd45cc44acf9932d6129fc7ad14994d7
Aled Brown <690500+aledbrown@users.noreply.github.com>
2024-10-15 19:46:53 +0100

    Delete working

--- app/Http/Controllers/Api/V1/AuthorTicketsController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketsController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
 use App\Http\Filters\V1\TicketFilter;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
-class AuthorTicketsController extends Controller
+class AuthorTicketsController extends ApiController
 {
     public function index($author_id, TicketFilter $filters)
     {

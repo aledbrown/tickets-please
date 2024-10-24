29e0f6eac25a4cf0d8ce64e4c657716491a49c36
Aled Brown <690500+aledbrown@users.noreply.github.com>
2024-10-24 07:32:49 +0100

    Update AuthorController.php

--- app/Http/Controllers/Api/V1/AuthorController.php
+++ app/Http/Controllers/Api/V1/AuthorController.php
@@ -12,7 +12,18 @@ class AuthorController extends ApiController
 {
     public function index(AuthorFilter $filters)
     {
-        return UserResource::collection(User::filter($filters)->paginate());
+        return UserResource::collection(
+            User::select('users.*')
+            ->join('tickets', 'users.id', '=', 'tickets.user_id')
+            ->filter($filters)
+            ->distinct()
+            ->paginate()
+        );
+
+
+        // return UserResource::collection(
+        //     User::filter($filters)->paginate()
+        // );
 
         // if ($this->include('tickets')) {
         //     return UserResource::collection(User::with('tickets')->paginate());
